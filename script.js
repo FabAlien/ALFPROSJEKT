40e7cfbef63ba830f75f2892971ca40608f971a3
FabAlien <fabalien.von@gmail.com>
2025-09-12 11:09:39 +0200

    Working Article Selector

--- script.js
+++ script.js
@@ -12,12 +12,22 @@ function burgerFunction() {
 
 async function getArticle() {
   const response = await fetch("articles.json");
-  const article = await response.json();
-  console.log(article)
+  const data = await response.json();
+  return data;
 }
 
+async function parseObject(index) {
+  const data = await getArticle();
+  const articles = data.articles;
+  const article0 = articles[index];
+  return article0;
+}
 
-
-getArticle();
-
-let article1 = article[0];
+async function updateArticle(articleNumber) {
+  const chosenArticle = await parseObject(articleNumber);
+  
+  document.getElementById("articleHeader").innerHTML = chosenArticle.header;
+  document.getElementById("articleParagraph0").innerHTML = chosenArticle.para1;
+  document.getElementById("articleParagraph1").innerHTML = chosenArticle.para2;
+  document.getElementById("articleParagraph2").innerHTML = chosenArticle.para3;
+}
